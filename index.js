dee5d38fb5fd21ee7b55b96d1b0b5370b73807a3
Tesfaye Hailu <tes.girmah@gmail.com>
2023-05-05 18:25:48 -0700

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //3.Create a class called "TodoList" with an array property to hold the list items and
 // methods to add and delete items from the list.
 
+
+//// Define the TodoList class with methods to add and delete items from the list
+
 class TodoList {
     constructor() {
       this.items = [];
@@ -15,20 +18,31 @@ class TodoList {
     }
   }
   
+  //Create an instance of the TodoList clas
+
   const todoList = new TodoList();
+
+  // Get references to the form, input, and list elements
   const form = document.getElementById('todo-form');
   const input = document.getElementById('todo-input');
   const list = document.getElementById('todo-list');
-  
+
+
+  // Add an event listener to the form's submit event
+
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const newItem = input.value.trim();
     if (newItem) {
       todoList.add(newItem);
       renderList();
+
+      // Clear the input field
       input.value = '';
     }
   });
+
+  // Define a function to render the list of todo items
   
   function renderList() {
     list.innerHTML = '';
